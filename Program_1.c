32c503a13d92872e8a6c1ddc582bad223bf13782
Gourav R <20cse0081@vvce.ac.in>
2021-02-19 11:10:11 +0000

    Counting number of vowles in a string

--- Program_1.c
+++ Program_1.c
@@ -3,11 +3,25 @@
 #include<stdio.h>
 
 int main()
-{
+{ int i, count=0;
 	char str[10];
 	scanf("%s", str);
 
 	//Write your code here
-
+  for(i=0;str[i]!=0;i++){
+    if( str[i]=='a' ||
+        str[i]=='A' ||
+        str[i]=='E' ||
+        str[i]=='I' ||
+        str[i]=='O' ||
+        str[i]=='U' ||
+        str[i]=='e' ||
+        str[i]=='i' ||
+        str[i]=='o' ||
+        str[i]=='u' )
+    { count++;
+    }
+  }
+printf("%d",count);
 	return 0;
 }
